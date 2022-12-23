72bf65401ead37fb763cb74722b359d85d8deec4
adrian <adrianlm1985@gmail.com>
2022-12-23 13:45:39 +0100

    EjercicioPOO

--- EjerciciosCSharpPOO/Cuenta.cs
+++ EjerciciosCSharpPOO/Cuenta.cs
@@ -6,6 +6,43 @@
         private string _titular;
         private double _cantidad;
 
+        //Propiedad
+        public string Titular { get => _titular; set => _titular = value; }
+        public double Cantidad { get => _cantidad; set => _cantidad = value; }
 
+        //Constructor
+        public Cuenta(string titular, double cantidad)
+        {
+            _titular = titular;
+            _cantidad = cantidad;
+        }
+
+        public Cuenta(string titular) : this (titular, 0)  //Debemos mandar dato dado en program o por defecto
+        {
+
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine($"{Titular}: {Cantidad}");
+        }
+
+        public void Ingresar(double cantidad)
+        {
+            if(0 > cantidad)
+            {
+                Cantidad += cantidad;
+            }
+        }
+
+        public void Retirar(double cantidad)
+        {
+            Cantidad -= cantidad;
+
+            if ( 0 < Cantidad)
+            {
+                Cantidad = 0;
+            }
+        }
     }
 }
