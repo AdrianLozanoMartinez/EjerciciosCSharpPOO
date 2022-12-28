70911e86b58a56ee26d730fbc007b2c956d3f3b4
adrian <adrianlm1985@gmail.com>
2022-12-28 22:01:13 +0100

    Ejercicio 5

---
+++ Ejercicio05/Persona.cs
@@ -0,0 +1,37 @@
+﻿namespace Ejercicio05
+{
+    class Persona
+    {
+        //Constante
+        const string LETTERS = "trwagmyfpdxbnjzsqvhlcke";
+
+        //Atributos
+        private string _nombre;
+        private int _edad;
+        private string _dni;
+
+        //Propiedades
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public int Edad { get => _edad; set => _edad = value; }
+        public string Dni { get => _dni; set => _dni = CalcularDNI(); }
+
+        //Constructores
+        public Persona(string nombre = "", int edad = 0, string dni = "")
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Dni = dni; //dni sin letra
+        }
+
+        public override string ToString()
+        {
+            return $"{_edad}";
+        }
+
+        public string CalcularDNI()
+        {
+            Dni
+            return "";
+        }
+    }
+}
---
+++ Ejercicio05/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ejercicio05
+{
+    class Program
+    {
+        static void Main()
+        {
+            Persona persona = new Persona();
+            Console.WriteLine(persona);
+        }
+    }
+}
