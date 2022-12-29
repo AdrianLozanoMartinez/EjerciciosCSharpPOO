3ea1498b4ea2e7c2a5482fe3b34a388ab69eb4c2
adrian <adrianlm1985@gmail.com>
2022-12-29 20:12:47 +0100

    Cambios cuenta Joven

---
+++ Ejercicio05/CuentaJoven.cs
@@ -0,0 +1,18 @@
+﻿namespace Ejercicio05
+{
+    class CuantaJoven
+    {
+        //Atributos
+        private string _headline;
+        private double _amount;
+        private int _bonus;
+
+        //Propiedades
+        public string Headline { get => _headline; set => _headline = value; }
+        public double Amount { get => _amount; set => _amount = value; }
+        public int Bonus { get => _bonus; set => _bonus = value; }
+
+        //Constructores
+
+    }
+}
