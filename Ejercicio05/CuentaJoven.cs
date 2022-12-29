2f4eb568b94444a60445d5dac902d5d259612aed
adrian <adrianlm1985@gmail.com>
2022-12-29 20:17:28 +0100

    Completando

--- Ejercicio05/CuentaJoven.cs
+++ Ejercicio05/CuentaJoven.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ejercicio05
 {
-    class CuantaJoven
+    class CuentaJoven : Persona
     {
         //Atributos
         private string _headline;
@@ -13,6 +13,23 @@
         public int Bonus { get => _bonus; set => _bonus = value; }
 
         //Constructores
+        public CuentaJoven(string headline, double amount, int bonus) 
+        { 
+            Headline = headline;
+            Amount = amount;
+            Bonus = bonus;
+        }
+        public CuentaJoven(string headline) : this (headline, 0, 0) { }
 
+        //Métodos
+        public void EsTitularValido()
+        {
+            bool isHeadline = false;
+
+            if (Age > 18 && Age < 25)
+            {
+                isHeadline = true;
+            }
+        }
     }
 }
